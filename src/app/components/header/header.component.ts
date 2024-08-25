f5fec152b1f6c809bfe108084b6107ba4bcf6361
sneika <fithisalma@gmail.com>
2024-08-25 14:52:37 +0100

    logOut

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { JwtHelperService } from '@auth0/angular-jwt';
+import { GuardService } from 'src/app/guards/services/guard.service';
 
 @Component({
   selector: 'app-header',
@@ -9,10 +10,17 @@ import { JwtHelperService } from '@auth0/angular-jwt';
 export class HeaderComponent {
   username;
 
-  constructor(private jwt: JwtHelperService){}
+  constructor(
+    private jwt: JwtHelperService,
+    private readonly authService: GuardService,
+  ){}
 
   ngOnInit(){
     const token = this.jwt.decodeToken(localStorage.getItem('token'));
     this.username = token.name;
+  }
+
+  logOut(){
+    this.authService.logout();
   }
 }
